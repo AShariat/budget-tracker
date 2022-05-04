c187b8f45a8f916394227912a5a60bddafe578b8
Amir Shariat <amirshariat1987@gmail.com>
2022-05-04 01:24:34 -0600

    added offline functionality to the app

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,34 +3,21 @@ const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
+  "/",
   "./index.html",
   "./css/style.css",
   "./js/index.js",
   "./js/idb.js",
-  "../routes/api.js",
-  "../models/transaction.js",
 ];
-
-self.addEventListener("fetch", function (e) {
-  console.log("Fetch Request : " + e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        console.log("Responding With Cache : " + e.request.url);
-        return request;
-      } else {
-        console.log("File Is Not Cached, Fetching : " + e.request.url);
-        return fetch(e.request);
-      }
-    })
-  );
-});
 
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       console.log("Installing Cache : " + CACHE_NAME);
-      return cache.addAll(FILES_TO_CACHE);
+      return cache
+        .addAll(FILES_TO_CACHE)
+        .then(() => console.log("Cached!"))
+        .catch((err) => console.log(err));
     })
   );
 });
@@ -50,6 +37,21 @@ self.addEventListener("activate", function (e) {
           }
         })
       );
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  console.log("Fetch Request : " + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log("Responding With Cache : " + e.request.url);
+        return request;
+      } else {
+        console.log("File Is Not Cached, Fetching : " + e.request.url);
+        return fetch(e.request);
+      }
     })
   );
 });
